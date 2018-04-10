83dd2ffd39e940666fe11aef65026424217bebd8
Sanjana Mundhra <sanjanamundhra@gmail.com>
2018-04-10 14:12:15 +0530

    quotes feature added

---
+++ src/reducers/quotes.js
@@ -0,0 +1,18 @@
+const quotesReducerDefaultState = [
+    {story:'First quote',isModerated:true,id:'sappppdasdasd'},
+    {story:'Second quote',isModerated:true,id:'asaskjlkjljkljdsdasd'},
+
+];
+const quotesReducer = (state = quotesReducerDefaultState, action) => {
+    switch (action.type){
+        case 'ADD_QUOTE':
+            return [
+                ...state,
+                action.quote
+            ];
+        default:
+            return state;
+    }
+};
+
+export default quotesReducer;
